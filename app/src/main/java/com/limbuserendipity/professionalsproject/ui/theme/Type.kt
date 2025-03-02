4a361529b7d17bc8538c7881b7213b62aec05e01
limbuserendipity <limbuserendipity@gmail.com>
2025-03-02 19:06:05 +0700

    update Typography x2

--- app/src/main/java/com/limbuserendipity/professionalsproject/ui/theme/Type.kt
+++ app/src/main/java/com/limbuserendipity/professionalsproject/ui/theme/Type.kt
@@ -2,33 +2,69 @@ package com.limbuserendipity.professionalsproject.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.limbuserendipity.professionalsproject.R
 
 // Set of Material typography styles to start with
+// https://developer.android.com/develop/ui/compose/designsystems/material3
+val newPeninimMtFont = Font(R.font.new_peninim_mt)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    displayLarge = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    displayMedium = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    displaySmall = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontFamily = FontFamily(newPeninimMtFont),
+        color = Text
+    ),
 )
