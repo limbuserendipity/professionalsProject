d21329072a4eec912cd7b89da91eac7cacfe6a89
limbuserendipity <limbuserendipity@gmail.com>
2025-03-02 17:20:56 +0700

    add file Icons with icons paths

---
+++ app/src/main/java/com/limbuserendipity/professionalsproject/ui/theme/Icons.kt
@@ -0,0 +1,16 @@
+package com.limbuserendipity.professionalsproject.ui.theme
+
+import com.limbuserendipity.professionalsproject.R
+
+// Просто файл с путями иконок для удобства
+
+val arrowDownPath = R.drawable.arrow_down
+val callPath = R.drawable.call
+val clockPath = R.drawable.clock
+val clock1Path = R.drawable.clock_1
+val emailPath = R.drawable.email
+val logoutPath = R.drawable.logout
+val markerPath = R.drawable.marker
+val marker1Path = R.drawable.marker_1
+val marker2Path = R.drawable.marker_2
+val settingsPath = R.drawable.settings
