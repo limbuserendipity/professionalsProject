2bcec5b96cdf30481bcfbacd34a481782d069a5d
limbuserendipity <limbuserendipity@gmail.com>
2025-03-03 14:42:47 +0700

    add SignInScreen

---
+++ app/src/main/java/com/limbuserendipity/professionalsproject/presentation/ui/screen/sign_in/SignInScreen.kt
@@ -0,0 +1,23 @@
+package com.limbuserendipity.professionalsproject.presentation.ui.screen.sign_in
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SignInScreen(){
+
+    Surface(
+
+    ) {
+        SignInContent()
+    }
+
+}
+
+@Composable
+fun SignInContent(){
+
+
+
+
+}
