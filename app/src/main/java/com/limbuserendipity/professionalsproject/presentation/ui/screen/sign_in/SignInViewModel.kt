d90ce64072e3ef9e6ee6da8a08472ee9aaa282c5
limbuserendipity <limbuserendipity@gmail.com>
2025-03-05 19:11:16 +0700

    add SignInViewModel

---
+++ app/src/main/java/com/limbuserendipity/professionalsproject/presentation/ui/screen/sign_in/SignInViewModel.kt
@@ -0,0 +1,70 @@
+package com.limbuserendipity.professionalsproject.presentation.ui.screen.sign_in
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class SignInViewModel : ViewModel(){
+
+    private val _state = MutableStateFlow(SignInState())
+    val state get() = _state
+
+    private fun updateState(newState : SignInState){
+        viewModelScope.launch {
+            _state.emit(newState)
+        }
+    }
+
+    fun updateEmail(email : String){
+        updateState(state.value.copy(email = email))
+    }
+
+    fun updatePassword(password : String){
+        updateState(state.value.copy(password = password))
+    }
+
+    fun authentication(){
+        val isEmailValid = checkEmailValidity(state.value.email)
+        val isEmptyString = checkEmptyString(state.value.email, state.value.password)
+    }
+
+    fun showPassword(){
+        updateState(state.value.copy(isPasswordVisible = !state.value.isPasswordVisible))
+    }
+
+    private fun checkEmailValidity(email : String): Boolean {
+        var isValid = true
+        val name = email.substringBefore('@')
+        val domainName = email
+            .substringAfter('@')
+            .substringBefore('.')
+        val highDomain = email.substringAfter('.')
+
+        isValid = name.all {
+            it.isLowerCase()
+        }
+        if(!isValid) return false
+
+        isValid = domainName.all{
+            it.isLowerCase()
+        }
+
+        if(!isValid) return false
+
+        isValid = highDomain.all{
+            it.isLetter()
+        }
+
+        if(!isValid) return false
+
+        isValid = highDomain.length > 2
+
+        return isValid
+    }
+
+    fun checkEmptyString(email: String, password: String) : Boolean{
+        return email.isEmpty() || password.isEmpty()
+    }
+
+}
