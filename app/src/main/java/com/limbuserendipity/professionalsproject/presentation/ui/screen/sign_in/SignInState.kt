14639ae72fd9094f40d7498c97efc2cf06119326
limbuserendipity <limbuserendipity@gmail.com>
2025-03-05 19:11:33 +0700

    add SignInState

---
+++ app/src/main/java/com/limbuserendipity/professionalsproject/presentation/ui/screen/sign_in/SignInState.kt
@@ -0,0 +1,9 @@
+package com.limbuserendipity.professionalsproject.presentation.ui.screen.sign_in
+
+
+data class SignInState(
+    var email : String = "",
+    var password : String = "",
+    var isEmailValid : Boolean? = null,
+    var isPasswordVisible : Boolean = false
+)
