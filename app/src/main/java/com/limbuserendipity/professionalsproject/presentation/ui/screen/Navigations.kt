62b7c6a7198298cdf6f1d2f060a3b7d9b5476954
limbuserendipity <limbuserendipity@gmail.com>
2025-03-05 19:12:27 +0700

    update Navigation

--- app/src/main/java/com/limbuserendipity/professionalsproject/presentation/ui/screen/Navigations.kt
+++ app/src/main/java/com/limbuserendipity/professionalsproject/presentation/ui/screen/Navigations.kt
@@ -1,18 +1,34 @@
 package com.limbuserendipity.professionalsproject.presentation.ui.screen
 
 import androidx.compose.runtime.Composable
-import com.limbuserendipity.professionalsproject.presentation.ui.screen.sign_in.SignInContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModelProvider
+import com.limbuserendipity.professionalsproject.presentation.ui.screen.sign_in.SignInScreen
+import com.limbuserendipity.professionalsproject.presentation.ui.screen.sign_in.SignInViewModel
 
 @Composable
-fun Navigation(){
-    val navState = NavState.SIGNIN
-    when(navState){
+fun Navigation(provider: ViewModelProvider) {
+    var navState by remember {
+        mutableStateOf(NavState.SIGNIN)
+    }
+    when (navState) {
         NavState.SIGNIN -> {
-            SignInContent()
+            SignInScreen(
+                viewmodel = provider.get(SignInViewModel::class.java),
+                toHomeScreen = {
+                    navState = NavState.HOME
+                }
+            )
+        }
+        NavState.HOME -> {
+
         }
     }
 }
 
-enum class NavState{
-    SIGNIN
+enum class NavState {
+    SIGNIN, HOME
 }
